58df81b1ee7c7e8907aecb034f04824f3893ab25
zhangxiaoan_v <zhangxiaoan_v@didiglobal.com>
2024-07-06 21:16:11 +0800

    feat: add proxy option.

--- deepE/docker/make/make_bcc.sh
+++ deepE/docker/make/make_bcc.sh
@@ -3,6 +3,8 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+#export HTTPS_PROXY=
+#git config --global http.proxy your_proxy
 #git config --global http.sslverify false
 git clone https://github.com/iovisor/bcc.git
 mkdir bcc/build; 
